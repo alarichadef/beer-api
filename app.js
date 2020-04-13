3a6ed65b93c7c74591a86d10dfc7a28a10be9d5a
alarichadef <alarichadef@gmail.com>
2020-04-13 19:15:11 +0200

    start working on api with add beers from csv and list them

---
+++ app.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const app = express();
+var multer  = require('multer');
+var upload = multer();
+
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alaric:bierebelge@cluster0-dstvq.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+  app.locals.db = client.db('beers-back');
+});
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+const onMakeBeers = (res, result) => {
+    res.status(200).json(result.ops);
+}
+
+//Clean the collection and create a new one from csv
+const makeBeers = (beers, callback) => {
+    const beersCollection = app.locals.db.collection('beers');
+    console.log('beers => ', beersCollection);
+    beersCollection.remove({},(err, result) => {
+        if (err)
+            console.log('err', err);
+        console.log('in remove callback', result)
+        beersCollection.insertMany(beers, (err, result) => {
+            callback(result);
+        });
+    });
+}
+
+app.post('/upload-beers', upload.single('file'), (req, res) => {
+    if (!req.file) {
+        res.status(500).send({ error: 'RIP' });
+    }
+    let fileCsv = req.file.buffer.toString('utf8');
+    let toInserts = [];
+    fileCsv.split('\n').map(line => {
+        columns = line.split(';');
+        if (columns.length === 6) {
+            let line = {
+                name: columns[0],
+                picture: columns[1],
+                brewery: columns[2],
+                alcohol: columns[3],
+                type: columns[4],
+                description: columns[5].trim()
+            }
+            toInserts.push(line);
+        }
+    });
+    makeBeers(toInserts, (result) => onMakeBeers(res, result));
+});
+
+app.get('/list-beers', (req, res) => {
+    const beersCollection = app.locals.db.collection('beers');
+    if (beersCollection) {
+        beersCollection.find({}).toArray((err, beers) => {
+            // so now, we can return all students to the screen.
+            res.status(200).json(beers);
+         });
+    } else {
+        res.status(500).send({error: 'RIP'});
+    }
+});
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
+})
