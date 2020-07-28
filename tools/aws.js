b98807ecf5986237413bc0b23670df4ecd790d7a
Alaric <alaric.hadef@weezevent.com>
2020-07-28 22:24:26 +0200

    fix aws credentials

--- tools/aws.js
+++ tools/aws.js
@@ -1,7 +1,7 @@
 const AWS = require("aws-sdk");
 const s3 = new AWS.S3({
-  accessKeyId: "AKIA6BXNIGA7ZEJZJOER", // aws access id here
-  secretAccessKey: "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", // aws secret access key here
+  accessKeyId: process.env.AWS_ACCESS_KEY, // aws access id here
+  secretAccessKey: process.env.AWS_SECRET, // aws secret access key here
   apiVersion: '2006-03-01',
   region: 'eu-west-3'
 });
