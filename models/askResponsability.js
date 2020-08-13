5ae2868f89b558712cf5d032373daf08f77ced9d
Alaric <alaric.hadef@weezevent.com>
2020-08-13 21:20:13 +0200

    feat(users): add bar/user in responsability and askresponsability list route

--- models/askResponsability.js
+++ models/askResponsability.js
@@ -22,11 +22,37 @@ class AskResponsability extends AskResponsabilityModel {
         return {id, reason, pictures, studied, accepted, userId, barId}
     }
 
-    static toListApi(barUsers) {
-		return barUsers.map(barUser =>
-			barUser.toApi()
-		);
-	}
+    static toAggregate(askResponsabilities) {
+        let aggregation_pipeline = [{
+			'$match': {id: {$in: askResponsabilities.map(askResponsability => askResponsability.id)}}
+		}];
+        aggregation_pipeline.push({
+			'$lookup': {
+				from: 'users',
+				localField: 'userId',
+				foreignField: 'id',
+				as: 'user'
+			}
+        }, {
+			'$lookup': {
+				from: 'bars',
+				localField: 'barId',
+				foreignField: 'id',
+				as: 'bar'
+			}
+        });
+        let cursor = AskResponsability.aggregate(aggregation_pipeline);
+		return cursor.toArray();
+    }
+
+    static toListApi(askResponsabilities) {
+        return askResponsabilities.map(askResponsability => {
+            let bar = Bar.fromJSON(askResponsability.bar)?.[0];
+            let user = User.fromJSON(askResponsability.user)?.[0];
+            return {id: askResponsability.id, bar: bar?.toApi() || null, user: user?.toApi() || null}
+        });
+    }
+
 }
 
 module.exports = AskResponsability;
